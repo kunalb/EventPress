b57e7402df83b327320e71a0c90d83f51639319c
Kunal Bhalla <bhalla.kunal@gmail.com>
2011-06-05 01:31:45 +0530

    Added initial version of the build file

---
+++ eventpress.php
@@ -0,0 +1,42 @@
+<?php
+/*
+	Plugin Name: EventPress
+	Plugin URI: http://github.com/kunalb/eventpress
+	Description: A complete events plugin for WordPress and BuddyPress.
+	Version: 0.2-bleeding
+	Author: Kunal Bhalla. 
+	Author URI: http://kunal-b.in
+	License: GPL2
+	Text Domain: eventpress
+
+	Copyright 2010 Kunal Bhalla  (email : bhalla.kunal@gmail.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as 
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+/**
+ * Loader. Set constants and load the main controller.
+ */
+
+// Define basic constant values
+
+// The current version of the plugin 
+define( 'EP_VERSION', '0.2-bleeding' );
+
+// The plugin directory, for including files, etc.
+define( 'EP_DIR', dirname(__FILE__) );
+
+// The URL of the plugin
+define( 'EP_REL_URL', plugins_url( $path = '/' . basename( dirname( __FILE__ ) ) ) );
