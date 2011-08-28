935002d883ec402ee3f6603b5ed676470dcc3aad
Kunal Bhalla <bhalla.kunal@gmail.com>
2011-08-29 01:11:59 +0530

    Updated the constants

--- constants.php
+++ constants.php
@@ -4,18 +4,25 @@
  * Constants
  */
 
-/**
- * The current version of the plugin 
- */
+/** The current version of the plugin */
 define( 'EP_VERSION', '0.2-bleeding' );
 
-/**
- * The plugin directory, for including files, etc.
- */
+/** The plugin directory, for including files, etc. */
 define( 'EP_DIR', dirname(__FILE__) );
 
-/**
- * The URL of the plugin
- */
+/** The URL of the plugin */
 define( 'EP_URL', plugins_url( $path = '/' . basename( dirname( __FILE__ ) ) ) );
+
+/** The default slug for an event. */
+define( 'EP_EVENT_SLUG', 'event' );
+
+/** The i18n version */
+define( 'EP_EVENT_I18N_SLUG', _x( 'event', 'Translated slug', 'eventpress' ) );
+
+/** The default slug for events. */
+define( 'EP_EVENTS_SLUG', 'events' );
+
+/** The i18n version for events. */
+define( 'EP_EVENTS_I18N_SLUG', _x( 'events', 'Translated slug', 'eventpress' ) );
+
 /**#@-*/
